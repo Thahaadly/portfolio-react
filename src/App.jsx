5b5997969d8f4e30140a9db034d02a2c54b82735
thahaadly <thahaadly@gmail.com>
2026-04-22 18:19:20 +0800

    Fix case sensitivity import in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import ProjectDetail from './pages/ProjectDetail';
